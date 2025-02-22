8c4e89a8826fda9abe8d226a8d2a3cf794ceb39c
satyam soni <satyamsoni853@gmail.com>
2025-02-22 18:06:49 +0530

    Done20

--- src/Education/Education.jsx
+++ src/Education/Education.jsx
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";
 import "./Education.css";
 import { motion } from "framer-motion";
 import { FaCode, FaSchool } from "react-icons/fa";
@@ -8,7 +8,12 @@ function Education() {
   return (
     <div className="education-main">
       <div className="education">
-        <p className="label heading text-conversion"  id="text-conversion-for-dark-mode">Get To Know More</p>
+        <p
+          className="label heading text-conversion"
+          id="text-conversion-for-dark-mode"
+        >
+          Get To Know More
+        </p>
         <p className="topic heading">Education</p>
         <div className="education-desc">
           <motion.div
@@ -17,31 +22,35 @@ function Education() {
             transition={{ duration: 0.5 }}
             className="college"
           >
-            
-            <FaSchool className="text-blue-500 text-2xl icon-style  hover:text-yellow-500" />
-            <p className="education-type">College</p> 
+            <IoSchoolOutline className="text-green-500 text-2xl icon-style  hover:text-yellow-500" />
+            <p className="education-type">School</p>
             <p>
-              Kamla Nehru Institute Of Physical And Social <br /> Sciences, Sultanpur, U.P
+              10th:{" "}
+              <span>
+                Maharishi Vidya Mandir, Sultanpur <br /> (CBSE Board)
+              </span>
             </p>
             <p>
-              BTech (CSE) <br /> (2021-2025)
+              12th:{" "}
+              <span>
+                Sangam Modern Inter College, Prayagraj <br /> (U.P Board)
+              </span>
             </p>
           </motion.div>
-
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             whileInView={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
             className="school"
           >
-            
-            <IoSchoolOutline className="text-green-500 text-2xl icon-style  hover:text-yellow-500" />
-            <p className="education-type">School</p>
+            <FaSchool className="text-blue-500 text-2xl icon-style  hover:text-yellow-500" />
+            <p className="education-type">College</p>
             <p>
-              10th: <span>Maharishi Vidya Mandir, Sultanpur <br /> (CBSE Board)</span>
+              Kamla Nehru Institute Of Physical And Social <br /> Sciences,
+              Sultanpur, U.P
             </p>
             <p>
-              12th: <span>Sangam Modern Inter College, Prayagraj <br /> (U.P Board)</span>
+              BTech (CSE) <br /> (2021-2025)
             </p>
           </motion.div>
         </div>
@@ -54,7 +63,6 @@ function Education() {
           transition={{ duration: 0.5 }}
           className="training"
         >
-          
           <FaCode className="text-red-500 text-2xl hover:text-yellow-500 icon-style" />
           <p className="education-type">Training</p>
           <p>
